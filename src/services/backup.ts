0ba80f4064e578bc9ce418353d0f6f5682388801
specialMinority <specialminority@gmail.com>
2026-01-26 01:53:20 +0900

    fix: import StorageAccessFramework from expo-file-system/legacy

--- src/services/backup.ts
+++ src/services/backup.ts
@@ -1,4 +1,5 @@
 import * as FileSystem from 'expo-file-system';
+import { StorageAccessFramework } from 'expo-file-system/legacy';
 import * as Sharing from 'expo-sharing';
 import * as DocumentPicker from 'expo-document-picker';
 import { Alert, Platform } from 'react-native';
